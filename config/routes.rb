326c0a112530847d7f4dcb56c614ac93c671b742
Harri Bell-Thomas <harribt@live.co.uk>
2014-08-14 17:01:09 +0100

    Added Users Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
+  get 'users/edit'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root 'houses#index'
