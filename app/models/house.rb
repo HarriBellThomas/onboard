5bc323be521cf39a8e1f1da4b72783595542f776
Harri Bell-Thomas <harribt@live.co.uk>
2014-08-15 15:19:47 +0100

    Add lat/long validation

--- app/models/house.rb
+++ app/models/house.rb
@@ -7,5 +7,8 @@ class House < ActiveRecord::Base
 
 	geocoded_by :address  # can also be an IP address
 	after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
+
+	validates :latitude, presence: true
+	validates :longitude, presence: true
 end
 
