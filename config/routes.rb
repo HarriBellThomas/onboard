6fc85abf8b7f554fa2368f91f7b3d9c4d5a39a96
Harri Bell-Thomas <harribt@live.co.uk>
2014-08-15 14:28:29 +0100

    Model relationships and New Order Path

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   resources :users do
   	resources :houses, only: [:show, :index]
+    resources :orders, only: [:show]
+
   end
   
   resource :session, only: [:new, :create, :destroy]
